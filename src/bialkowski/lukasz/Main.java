309ab4c97722a7239b54a0dd2f5484f7275867d3
Lukasz Bialkowski <lukasz.adrian.bialkowski@gmail.com>
2016-03-13 00:51:14 +0100

    All key features implemented. Main loop/ Printing results/ Bruteforce TODO

--- src/bialkowski/lukasz/Main.java
+++ src/bialkowski/lukasz/Main.java
@@ -4,15 +4,15 @@ import bialkowski.lukasz.globals.StaticVariables;
 import bialkowski.lukasz.services.FileReaderService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 public class Main {
 
     private Graph graph;
-    private int[] bestChromosome = null;
-    private double bestQualityScore = -1;
     private List<int[]> population = new ArrayList<>();
+    private boolean solutionFound = false;
 
     public static void main(String args[]) {
         Main graphColoringProblem = new Main();
@@ -33,6 +33,136 @@ public class Main {
         population = initializePopulation(graph.getMyNumVertices(), StaticVariables.COLOURS_COUNT, StaticVariables.POPULATION_SIZE);
         printPopulation(population);
         holePopulationQuality(population);
+
+        testujAlgorytm();
+    }
+
+    public void algorithm() {
+
+    }
+
+    private void testujAlgorytm() {
+        /*TESTOWANIE MUTACJI
+        List<int[]> trysome = new ArrayList<>();
+        trysome.add(new int[]{1,2,3,4,5,6,7});
+        trysome.add(new int[]{1,2,3,4,5,6,7});
+        trysome.add(new int[]{1,2,3,4,5,6,7});
+        trysome.add(new int[]{1,2,3,4,5,6,7});
+        trysome.add(new int[]{1,2,3,4,5,6,7});
+        trysome.add(new int[]{1,2,3,4,5,6,7});
+        System.out.println("Przed mutacja");
+        this.printPopulation(trysome);
+        this.mutatePopulation(trysome);
+        System.out.println("Po mutacji");
+        this.printPopulation(trysome);*/
+
+
+        /*TESTOWANIE SELEKCJI
+        List<int[]> selection1 = selection(population);
+        holePopulationQuality(selection1);
+        List<int[]> selection2 = selection(selection1);
+        holePopulationQuality(selection2);
+        List<int[]> selection3 = selection(selection2);
+        holePopulationQuality(selection3);*/
+
+        /* TESTOWANIE KRZYZOWANIA
+        List<int[]> trysome = new ArrayList<>();
+        trysome.add(new int[]{1,1,1,1,1,1,1});
+        trysome.add(new int[]{2,2,2,2,2,2,2});
+        trysome.add(new int[]{3,3,3,3,3,3,3});
+        trysome.add(new int[]{4,4,4,4,4,4,4});
+        trysome.add(new int[]{5,5,5,5,5,5,5});
+        trysome.add(new int[]{6,6,6,6,6,6,6});
+        System.out.println("Przed crossover");
+        for (int i = 0; i < trysome.size(); i++) {
+            int[] ints = trysome.get(i);
+            System.out.print("Chromosome "+i+": ");
+            for (int j = 0; j < ints.length; j++) {
+                System.out.print(ints[j]+ " ");
+            }
+            System.out.println();
+        }
+        crossover(trysome);
+        System.out.println("Po crossover");
+        for (int i = 0; i < trysome.size(); i++) {
+            int[] ints = trysome.get(i);
+            System.out.print("Chromosome "+i+": ");
+            for (int j = 0; j < ints.length; j++) {
+                System.out.print(ints[j]+ " ");
+            }
+            System.out.println();
+        }*/
+    }
+
+    private List<int[]> selection(List<int[]> currentPopulation) {
+        List<int[]> newPopulation = new ArrayList<>();
+        int newPopulationSize = 0;
+        int currentPopulationSize = currentPopulation.size();
+        HashSet<Integer> competitors;
+        List<int[]> competitorsChromosomes = new ArrayList<>();
+
+        while (newPopulationSize < StaticVariables.POPULATION_SIZE) {
+
+            competitors = new HashSet<>();
+            for (int i = 0; i < StaticVariables.SELECTION_COMPETITION_SIZE; i++) {
+                while (!competitors.add((int) (Math.random() * currentPopulationSize))) {}
+            }
+
+            for (Integer i : competitors) {
+                competitorsChromosomes.add(currentPopulation.get(i));
+            }
+
+            int[] competitionWinner = getCompetitionWinner(competitorsChromosomes);
+            int[] array = new int[competitionWinner.length];
+            System.arraycopy( competitionWinner, 0, array, 0, competitionWinner.length );
+
+            newPopulation.add(array);
+            newPopulationSize++;
+        }
+        return newPopulation;
+    }
+
+    private int[] getCompetitionWinner(List<int[]> competitorsChromosomes) {
+        int bestIndex = -1;
+        double bestQuality = -1;
+
+        if(competitorsChromosomes.size()>0){
+            bestQuality = qualityFunction(competitorsChromosomes.get(0));
+            bestIndex = 0;
+        }
+
+        for (int i = 1; i<competitorsChromosomes.size(); i++) {
+            double quality = qualityFunction(competitorsChromosomes.get(i));
+            if (quality < bestQuality) {
+                bestQuality = quality;
+                bestIndex = i;
+            }
+        }
+
+        return competitorsChromosomes.get(bestIndex);
+    }
+
+    private void mutatePopulation(List<int[]> population) {
+        int geneToMutate = -1;
+        int temp = -1;
+        for (int i = 0; i < population.size(); i++) {
+            int[] chromosome = population.get(i);
+
+            for (int j = 0; j < chromosome.length; j++) {
+                if (Math.random() < StaticVariables.MUTATION_POSSIBILITY) {
+                    if(geneToMutate!=-1){
+                        temp = chromosome[j];
+                        chromosome[j] = chromosome[geneToMutate];
+                        chromosome[geneToMutate] = temp;
+                        temp = -1;
+                        geneToMutate = -1;
+                    } else{
+                        geneToMutate = j;
+                    }
+                }
+
+            }
+        }
     }
 
     private List<int[]> initializePopulation(int myNumVertices, int maxColor, int populationCount) {
@@ -56,12 +186,93 @@ public class Main {
         return population;
     }
 
+    private void crossover(List<int[]> population){
+        boolean isOdd = population.size()%2==0;
+        double probabilityArray[] = isOdd?new double[population.size()/2]:new double[(population.size()-1)/2];
+        probabilityArray = generateProbabilityArray(probabilityArray);
+        int lastChromosomeIndex = population.size()-1;
+        List<int[]> newChromosomes = new ArrayList<>();
+        List<int[]> returnedChromosomes=new ArrayList<>();
+
+        for (int i = 0; i < probabilityArray.length; i++) {
+            if(probabilityArray[i] <= StaticVariables.CROSSOVER_POSSIBILITY ){
+                returnedChromosomes = cossoverTwoChromosomes(population.get(i), population.get(population.size()-1-i));
+            }
+            if(returnedChromosomes.size()>2){
+                System.out.println("KLAJSLKJASKFLJAKJF:LASJF");
+            }
+            for (int j = 0; j < returnedChromosomes.size(); j++) {
+                newChromosomes.add(returnedChromosomes.get(j));
+            }
+        }
+
+        population.addAll(newChromosomes);
+    }
+
+    private List<int[]> cossoverTwoChromosomes(int[] firstChromosome, int[] secondChromosome) {
+        int maxCrossingIndex = (int) firstChromosome.length / 2;
+        int tempValue;
+        int chromosomeLength = firstChromosome.length;
+
+        /**
+         * */
+        printArray(firstChromosome);
+        System.out.print(  " mutuje z ");
+        printArray(secondChromosome);
+        /**
+         * */
+
+        int[] newChromosome1 = new int[chromosomeLength];
+        System.arraycopy( firstChromosome, 0, newChromosome1, 0, firstChromosome.length );
+        int[] newChromosome2 = new int[chromosomeLength];
+        System.arraycopy( secondChromosome, 0, newChromosome2, 0, secondChromosome.length );
+
+        for (int i = 0; i < maxCrossingIndex; i++) {
+            tempValue = newChromosome1[i];
+            newChromosome1[i] = newChromosome2[i];
+            newChromosome2[i] = tempValue;
+        }
+        List<int[]> newChromosomes = new ArrayList<>();
+        newChromosomes.add(newChromosome1);
+        newChromosomes.add(newChromosome2);
+
+        return newChromosomes;
+    }
+
+    private void printArray(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private double[] generateProbabilityArray(double[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=Math.random();
+        }
+        return arr;
+    }
+
     private double qualityFunction(int[] chromosome){
         double result = 0;
         int collisionCount = 0;
+        int minColor = -1;
+        int maxColor = -1;
+
+        if (chromosome.length > 0) {
+            minColor = chromosome[0];
+            maxColor = chromosome[0];
+        }
 
         for (int i = 0; i < chromosome.length; i++) {
             int currentColor = chromosome[i];
+            if (currentColor > maxColor) {
+                maxColor = currentColor;
+            }
+            if (currentColor < minColor) {
+                minColor = currentColor;
+            }
             HashSet<Edge> edges = graph.getMyAdjList().get(i + 1);
 
             for (Edge edge : edges) {
@@ -72,8 +283,14 @@ public class Main {
                     collisionCount++;
             }
         }
+        int colorNumber = (maxColor-minColor+1);
+        double finalScore = (collisionCount+1)*colorNumber;
 
-        return collisionCount;
+        if(collisionCount == 0){
+            this.solutionFound = true;
+        }
+
+        return finalScore;
     }
 
     private boolean isInvalid(int weight, int v1, int v2) {
@@ -88,47 +305,60 @@ public class Main {
             for (int j = 0; j <ints.length; j++) {
                 System.out.print(ints[j] + " ");
             }
+            System.out.println();
 
-            System.out.println(" = > "+ qualityFunction(ints));
+//            System.out.println(" = > "+ qualityFunction(ints));
         }
     }
 
     private void holePopulationQuality(List<int[]> population){
         double quality = 0;
-        for (int[] chromosome : population) {
+        double bestPopulationScore = -1;
+        double worstPopulationScore = -1;
+        double averagePopulationScore;
+        int[] bestChromosomeInPopulation = null;
+        int[] worstChromosomeInPopulation = null;
+
+        if (population.size() > 0) {
+            double initial = qualityFunction(population.get(0));
+            quality+=initial;
+            bestPopulationScore = initial;
+            worstPopulationScore = initial;
+            bestChromosomeInPopulation = population.get(0);
+            worstChromosomeInPopulation = population.get(0);
+        }
+
+        for (int i = 1; i < population.size(); i++) {
+            int[] chromosome = population.get(i);
             double result = qualityFunction(chromosome);
 
-            if(this.bestQualityScore == -1){
-                this.bestQualityScore = result;
+            if (result < bestPopulationScore) {
+                bestPopulationScore = result;
+                bestChromosomeInPopulation = chromosome;
             }
-
-            if(result < this.bestQualityScore){
-                this.bestQualityScore = result;
-                this.bestChromosome = chromosome;
+            if (result > worstPopulationScore) {
+                worstPopulationScore = result;
+                worstChromosomeInPopulation = chromosome;
             }
-            quality+=result;
+            quality += result;
         }
+
         System.out.println("Wartosc sumaryczna bledow w calej populacji: " + quality);
-        System.out.println("Najlepsza wartosc funkcji: " + this.bestQualityScore);
-        for (int i : this.bestChromosome) {
+        System.out.println("Najlepsza wartosc funkcji: " + bestPopulationScore);
+        System.out.print("Wartosci chromosomu najlepszego: ");
+        for (int i : bestChromosomeInPopulation) {
             System.out.print(i+" ");
         }
         System.out.println();
-    }
 
-    public int[] getBestChromosome() {
-        return bestChromosome;
-    }
+        System.out.println("Najgorszy wartosc funkcji: " + worstPopulationScore);
+        System.out.print("Wartosci chromosomu najgorszego: ");
+        for (int i : worstChromosomeInPopulation) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
 
-    public void setBestChromosome(int[] bestChromosome) {
-        this.bestChromosome = bestChromosome;
-    }
-
-    public double getBestQualityScore() {
-        return bestQualityScore;
-    }
-
-    public void setBestQualityScore(double bestQualityScore) {
-        this.bestQualityScore = bestQualityScore;
+        averagePopulationScore = quality / StaticVariables.POPULATION_SIZE;
+        System.out.println("Srednia wartosc bledu w populacji: "+averagePopulationScore);
     }
 }
--- src/bialkowski/lukasz/globals/StaticVariables.java
+++ src/bialkowski/lukasz/globals/StaticVariables.java
@@ -3,9 +3,9 @@ package bialkowski.lukasz.globals;
 public class StaticVariables {
 
     public static final int POPULATION_SIZE = 1000;
-    public static final double MUTATION_POSSIBILITY = 0.01;
-    public static final double CROSSOVER_POSSIBILITY = 0.6;
-    public static final int SELECTION_COMPETITION_SIZE = 4;
-    public static final int COLOURS_COUNT = 20;
+    public static final double MUTATION_POSSIBILITY = 0.1;
+    public static final double CROSSOVER_POSSIBILITY = 1;
+    public static final int SELECTION_COMPETITION_SIZE = 10;
+    public static final int COLOURS_COUNT = 30;
 
 }
